aba7a719bb0491733df8f09f3585b5f0bbf08a4c
seoyoon lee <leeseoyoon01@gmail.com>
2026-02-10 22:05:55 +0900

    feat: 라우터, 레이아웃 설정

---
+++ src/router/router.jsx
@@ -0,0 +1,37 @@
+import {createBrowserRouter} from "react-router-dom";
+import RootLayout from "../layout/RootLayout";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <RootLayout/>,
+        errorElement: <h1>에러 페이지</h1>,
+        children:[
+            {
+                index:true,
+                element:<h1>홈 페이지</h1>
+            },
+            {
+                path: 'login',
+                element: <h1>로그인 페이지</h1>
+            },
+            {
+                path: 'signup',
+                element: <h1>회원가입 페이지</h1>
+            },
+            {
+                path: 'home',
+                element: <h1>홈 페이지</h1>
+            },
+            {
+                path: 'recommend',
+                element: <h1>추천 편지 페이지</h1>
+            },
+            {
+                path: 'mypage',
+                element: <h1>마이 페이지</h1>
+            }]
+    }
+])
+
+export default router
