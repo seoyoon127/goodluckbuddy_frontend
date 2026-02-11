39e47e39e028d097256218c78871262a6c333bc1
seoyoon lee <leeseoyoon01@gmail.com>
2026-02-11 20:42:56 +0900

    feat: roundWhiteButton 구현 #2

---
+++ src/components/RoundWhiteButton.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const RoundWhiteButton = ({text}) => {
+    return (
+        <>
+            <Button>{text}</Button>
+        </>
+    )
+}
+export default RoundWhiteButton;
+
+const Button = styled.button`
+    font-size: 12px;
+    padding: 3px 7px;
+    border-radius: 999px;
+
+    border: 1px solid #68AB46;
+    color: #68AB46;
+    background-color: white;
+
+    cursor: pointer;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
