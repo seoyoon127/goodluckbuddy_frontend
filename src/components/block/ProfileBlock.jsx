b4bf2ef711a94e97f0b2de6ee1f66afea533ecd0
seoyoon lee <leeseoyoon01@gmail.com>
2026-02-14 20:34:19 +0900

    feat: 마이페이지 구현 #6

---
+++ src/components/block/ProfileBlock.jsx
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+import RoundWhiteButton from "../button/RoundWhiteButton";
+import IconSrc from "../../assets/icon.png"
+import ProfileTitle from "../text/ProfileTitle";
+
+const ProfileBlock = ({nickname, gender, ageGroup, interest, onClick}) => {
+    return (
+        <>
+            <Block onClick={onClick}>
+                <Wrapper> 
+                    <Image src={IconSrc} alt="icon"/>
+                    <ProfileTitle textGreen={nickname} text={`\n${gender}/${ageGroup}/관심분야:${interest}`}/>
+                </Wrapper>
+                <ButtonPosition>
+                    <RoundWhiteButton text={"프로필 정보 수정 ➜"} width="250px"/>
+                </ButtonPosition>
+            </Block>
+        </>
+    )
+}
+export default ProfileBlock;
+
+const Block = styled.div`
+    width: 300px;
+    height: 100px;
+    background: #BEE9A8;
+    border-radius: 5px;
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    padding:10px;
+`;
+
+const Wrapper = styled.div`
+    width: 90%;
+    height: 60px;
+    display: flex;
+    justify-content: flex-start; 
+    align-items: center;
+    gap: 10px;
+    margin-bottom: 10px;
+`;
+
+const Image = styled.img`
+    width: 75px;
+    heigth: 75px;
+    margin-top: 30px;
+    margin-bottom: 30px;
+`;
+
+const ButtonPosition = styled.div`
+    width: 100%;
+    height: 60px;
+    display: flex;
+    justify-content: center;
+`;
