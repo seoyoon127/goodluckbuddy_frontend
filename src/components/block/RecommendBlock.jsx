e19b6387889e470ee752d2523f75eb5b02be43eb
seoyoon lee <leeseoyoon01@gmail.com>
2026-02-15 17:49:58 +0900

    style: Block 가운데 정렬 적용

--- src/components/block/RecommendBlock.jsx
+++ src/components/block/RecommendBlock.jsx
@@ -35,7 +35,7 @@ const Wrapper = styled.div`
     width: 100%;
     height: 60px;
     display: flex;
-    justify-content: flex-start; 
+    justify-content: center; 
     align-items: center;
     gap: 10px;
     margin-bottom: 10px;
