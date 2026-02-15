35fd7d2f1776aae4ca5ab320feedf7c4849ccf68
seoyoon lee <leeseoyoon01@gmail.com>
2026-02-15 17:22:03 +0900

    feat: Letter 리스트 레이아웃, 좋아요한 편지보기 페이지 구현 #8

---
+++ src/pages/MyLikesPage.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import Navbar from "../components/navbar/navbar";
+import LetterList from "../layout/LetterListLayout";
+
+
+const MyLikesPage = () => {
+    return (
+        <>
+            <Page>
+                <Navbar title={"좋아요한 편지"}/>
+                <LetterList/>
+            </Page>
+        </>
+    )
+}
+
+export default MyLikesPage;
+
+const Page = styled.div`
+     min-height: calc(100vh - 80px);
+    display: flex;
+    flex-direction: column;
+`;
