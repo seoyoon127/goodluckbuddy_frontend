23d38d9a6511627ca12e06403cea2d99c379dea3
seoyoon lee <leeseoyoon01@gmail.com>
2026-02-20 23:16:27 +0900

    feat: 로그아웃 구현 #10

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -5,17 +5,20 @@ import { useNavigate } from "react-router-dom"
 import SubTitle from "../components/text/SubTitle"
 import SquareGreenLongButton from "../components/button/SquareGreenLongButton"
 import SquareWhiteLongButton from "../components/button/SquareWhiteLongButton"
+import usePostLogout from "../apis/usePostLogout"
 
 const MyPage = () => {
+    const { mutate: postLogout } = usePostLogout();
     const navigate = useNavigate();
     const handleLogout = () => {
-        // 로그아웃 로직
+        console.log("click")
+        postLogout();
     }
     return (
         <>
             <Navbar title={"마이페이지"} mypage={false}/>
             <Wrapper>
-                <ProfileBlock nickname={"닉네임"} gender={"여성"} ageGroup={"20대"} interest={"가족"} my={true} onClick={()=>navigate("/my/profile")}/>
+                <ProfileBlock nickname={"닉네임"} gender={"여성"} ageGroup={"20대"} interest={"가족"} $my={true} onClick={()=>navigate("/my/profile")}/>
                 <ContentWrapper>
                     <TextWrapper><SubTitle textE={"내 활동"}/></TextWrapper>
                     <SquareGreenLongButton text={"좋아요한 편지 보기"} width="250px" onClick={()=>navigate("/my/likes")}/>
