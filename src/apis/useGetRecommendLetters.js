8081f495d9484c0a32f72290e42b7ab632cfd59c
seoyoon lee <leeseoyoon01@gmail.com>
2026-02-24 21:52:57 +0900

    feat: 추천 편지 조회 api 연동 #14

---
+++ src/apis/useGetRecommendLetters.js
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import axiosInstance from "./axiosInstance";
+
+const useGetRecommendLetters = () => {
+    return useQuery({
+        queryKey: ["recommendLetters"],
+        queryFn: async () => {
+            const response = await axiosInstance.get(`/api/letters/recommend`);
+            return response.data.result.letters ?? [];
+        },
+    });
+}
+
+export default useGetRecommendLetters;
